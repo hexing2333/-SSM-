4a6ca3b74ec27e11b7a422c3d0a89f85f9f44f22
hexing2333 <1418226938@qq.com>
2021-01-13 15:54:35 +0800

    version2.0

--- src/main/java/com/liaocheng/archives/Po/subject.java
+++ src/main/java/com/liaocheng/archives/Po/subject.java
@@ -3,6 +3,8 @@ package com.liaocheng.archives.Po;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.sql.Date;
+
 @Data
 //学习成绩类 包含成绩ID 课程ID和名称 学生ID和名称 成绩
 public class subject implements Serializable {
@@ -12,4 +14,6 @@ public class subject implements Serializable {
     private Integer sID;
     private String sName;
     private Integer score;
+    private Date eTime;
+    private java.util.Date createTime;
 }
